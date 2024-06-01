9226dffc815ccbe6a7116b333f2082e4f19249b7
Alihan Gündoğdu <alihangundogdu01@gmail.com>
2024-06-02 02:50:44 +0300

    direction enum

--- src/Snake.java
+++ src/Snake.java
@@ -2,8 +2,14 @@ import java.awt.*;
 import java.util.Stack;
 
 public class Snake {
+    enum Direction{
+        LEFT, RIGHT, UP, DOWN
+    }
+
     private int speed;
     private Stack<Point> snakeTail = new Stack<Point>();
+    private Direction direction = Direction.RIGHT;
+
 
     Snake(int x, int y, int speed) {
         snakeTail.push(new Point(x, y));
