33b7a36026c6b26603e5e3e1ee548a79c05d68f5
Alihan Gündoğdu <alihangundogdu01@gmail.com>
2024-06-02 03:33:58 +0300

    changes snake direction for user input

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.security.Key;
 
 public class MainPanel extends JFrame
 {
@@ -9,7 +11,7 @@ public class MainPanel extends JFrame
     int rows = 50;
     int cols = 50;
     Canvas canvas = new Canvas(rows,cols);
-
+    Snake snake = new Snake(5,5,1);
     MainPanel(){
         JPanel panel = new JPanel();
 
@@ -17,9 +19,11 @@ public class MainPanel extends JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800,800);
 
+        addKeyListener(new SnakeAlgorithm(snake,canvas));
 
         setVisible(true);
     }
 
-
 }
+
+
--- src/Snake.java
+++ src/Snake.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 public class Snake {
@@ -12,6 +14,21 @@ public class Snake {
     private Stack<Point> snakeTail = new Stack<Point>();
     private Direction direction = Direction.RIGHT;
 
+    Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while(true){
+                try {
+                    Thread.sleep(1000/speed);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                changeSnakeLocation();
+                paintSnake();
+
+            }
+        }
+    });
 
     Snake(int x, int y, int speed) {
         snakeTail.push(new Point(x, y));
@@ -34,5 +51,55 @@ public class Snake {
         this.direction = direction;
     }
 
+    private void changeSnakeLocation(){
+        int deltaX = direction == Direction.RIGHT ? 1 : -1;
+        int deltaY = direction == Direction.UP ? 1 : -1;
+
+        snakeTail.pop();
+        snakeTail.push(new Point(snakeTail.peek().x + deltaX, snakeTail.peek().y + deltaY));
+    }
+
+    private void paintSnake(){
+
+    }
+}
+
+
+class SnakeAlgorithm implements KeyListener {
+
+
+    Snake snake;
+
+    SnakeAlgorithm(Snake snake,Canvas canvas){
+        this.snake = snake;
+
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        switch (e.getKeyChar()){
+            case 's':
+                snake.setDirection(Snake.Direction.DOWN);
+                break;
+            case 'd':
+                snake.setDirection(Snake.Direction.RIGHT);
+                break;
+            case 'w':
+                snake.setDirection(Snake.Direction.UP);
+                break;
+            case 'a':
+                snake.setDirection(Snake.Direction.LEFT);
+        }
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
 
